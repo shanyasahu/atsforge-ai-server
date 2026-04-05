a84d06cba08e0ebcf9ae5a0b1fdf62dcfde76abd
Shanya-Sahu <shanyasahu11@gmail.com>
2026-04-05 20:36:35 +0530

    add cors and update some functionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 //initate the server & create instance of server and use api route here
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 //require all the routes here
 const authRouter = require("./routes/auth.routes");
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -9,61 +9,69 @@ const tokenBlacklistModel = require("../models/blacklist.model");
  * @access Public
  */
 async function registerUserController(req, res) {
-  const { username, email, password } = req.body; //destructure from req.body
+  try {
+    const { username, email, password } = req.body; //destructure from req.body
 
-  if (!username || !email || !password) {
-    return res.status(400).json({
-      success: false,
-      message: "Please enter the username, email and password!",
-    });
-  }
-
-  //if username or email already exists
-
-  // $or operator ask the array in which we put the multiple condtions if any one condtion is true return the statement
-
-  const isUserAlreadyExists = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
-
-  if (isUserAlreadyExists) {
-    if (isUserAlreadyExists.username == username) {
+    if (!username || !email || !password) {
       return res.status(400).json({
-        message: "Account already exists with this username!",
-      });
-    } else {
-      return res.status(400).json({
-        message: "Account already exists with this email address!",
+        success: false,
+        message: "Please enter the username, email and password!",
       });
     }
+
+    //if username or email already exists
+
+    // $or operator ask the array in which we put the multiple condtions if any one condtion is true return the statement
+
+    const isUserAlreadyExists = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (isUserAlreadyExists) {
+      if (isUserAlreadyExists.username == username) {
+        return res.status(400).json({
+          message: "Account already exists with this username!",
+        });
+      } else {
+        return res.status(400).json({
+          message: "Account already exists with this email address!",
+        });
+      }
+    }
+
+    //bcrypt the password using hasing
+    const hash = await bcrypt.hash(password, 10);
+
+    //create new user
+    const user = await userModel.create({
+      username,
+      email,
+      password: hash,
+    });
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+      message: "User successfully registerd!",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
-
-  //bcrypt the password using hasing
-  const hash = await bcrypt.hash(password, 10);
-
-  //create new user
-  const user = await userModel.create({
-    username,
-    email,
-    password: hash,
-  });
-
-  const token = jwt.sign(
-    { id: user._id, username: user.username },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
-
-  res.cookie("token", token);
-
-  res.status(201).json({
-    message: "User successfully registerd!",
-    user: {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-    },
-  });
 }
 
 /**
@@ -73,52 +81,60 @@ async function registerUserController(req, res) {
  */
 
 async function loginUserController(req, res) {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await userModel.findOne({ email });
+    const user = await userModel.findOne({ email });
 
-  if (!user) {
-    return res.status(400).json({
-      message: "Invalid email or password",
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid password",
+      });
+    }
+
+    // const token = jwt.sign(
+    //   { id: user._id, username: user.username },
+    //   process.env.JWT_SECRET,
+    //   { expiresIn: "1d" },
+    // );
+
+    // res.cookie("token", token);
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully!",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
     });
   }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) {
-    return res.status(400).json({
-      message: "Invalid password",
-    });
-  }
-
-  // const token = jwt.sign(
-  //   { id: user._id, username: user.username },
-  //   process.env.JWT_SECRET,
-  //   { expiresIn: "1d" },
-  // );
-
-  // res.cookie("token", token);
-
-  const token = jwt.sign(
-    { id: user._id, username: user.username },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "lax",
-    path: "/",
-  });
-
-  res.status(200).json({
-    message: "User logged in successfully!",
-    user: {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-    },
-  });
 }
 
 /**
@@ -128,25 +144,33 @@ async function loginUserController(req, res) {
  */
 
 async function logoutUserController(req, res) {
-  const token = req.cookies?.token;
+  try {
+    const token = req.cookies?.token;
 
-  const blacklisted = await tokenBlacklistModel.findOne({ token });
+    const blacklisted = await tokenBlacklistModel.findOne({ token });
 
-  if (blacklisted) {
-    return res.status(401).json({
-      message: "Token expired. Please login again.",
+    if (blacklisted) {
+      return res.status(401).json({
+        message: "Token expired. Please login again.",
+      });
+    }
+
+    if (token) {
+      const saved = await tokenBlacklistModel.create({ token });
+    }
+
+    res.clearCookie("token");
+
+    res.status(200).json({
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
     });
   }
-
-  if (token) {
-    const saved = await tokenBlacklistModel.create({ token });
-  }
-
-  res.clearCookie("token");
-
-  res.status(200).json({
-    message: "User logged out successfully",
-  });
 }
 
 /**
@@ -156,16 +180,24 @@ async function logoutUserController(req, res) {
  */
 
 async function getMeController(req, res) {
-  const user = await userModel.findById(req.user.id);
+  try {
+    const user = await userModel.findById(req.user.id);
 
-  res.status(200).json({
-    message: "User details fetched successfully!",
-    user: {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-    },
-  });
+    res.status(200).json({
+      message: "User details fetched successfully!",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 }
 
 module.exports = {
